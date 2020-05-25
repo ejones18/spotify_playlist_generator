7445767c527f421721c212cb6971d31fd877980c
= <ejones18@sheffield.ac.uk>
2020-05-25 22:19:46 +0100

    Initial commit

---
+++ playlist_recommendations.py
@@ -0,0 +1,96 @@
+"""
+A python script that fetches song recommendations using the Spotify API
+- Credit rob_med from medium.com
+- Ethan Jones <ejones18@sheffield.ac.uk>
+- First authored: 2020-05-25
+"""
+import argparse
+import random
+
+import requests
+
+def main(seed_file):
+    """
+    Print a 25 song-long playlist based on a randomly chosen input song
+    """
+    seeds = randomly_get_seed_from_file(seed_file)
+    query_api(seeds)
+
+def define_settings():
+    """
+    Sets the endpoint as well as defines the user_id and the token
+    """
+    endpoint_url = "https://api.spotify.com/v1/recommendations?"
+    token = ""
+    user_id = ""
+    settings = [endpoint_url, token, user_id]
+    return settings
+
+def define_filters(seeds):
+    """
+    Sets the filters i.e. number of songs and genre
+    """
+    limit = 25
+    market = "GB"
+    seed_genres = "indie"
+    seed_artists = seeds[0]
+    seed_tracks = seeds[1]
+    filters = [limit, market, seed_genres, seed_artists, seed_tracks]
+    return filters
+
+def randomly_get_seed_from_file(seed_file):
+    """
+    Reads the input file and fetches a random set of seeds.
+    Seeds found @ https://developer.spotify.com/console/get-search-item/
+    """
+    file = open(seed_file, "r")
+    lines = file.readlines()
+    line = []
+    for index in range(0, len(lines)-1):
+        selected_line = lines[index]
+        line_length = len(selected_line)
+        sub_string = selected_line[:line_length-1]
+        line.append(sub_string)
+    line.append(lines[index+1])
+    seeds = random.choice(line).split()
+    return seeds
+
+def query_api(seeds):
+    """
+    Queries the Spotify API and returns a json response
+    """
+    settings = define_settings()
+    filters = define_filters(seeds)
+    query = f'{settings[0]}limit={filters[0]}&market={filters[1]}&seed_genres={filters[2]}'
+    query += f'&seed_artists={filters[3]}'
+    query += f'&seed_tracks={filters[4]}'
+    response = requests.get(query,
+                            headers={"Content-Type":"application/json",
+                                     "Authorization":f"Bearer {settings[1]}"})
+    json_response = response.json()
+    print_output(json_response)
+
+def print_output(json_response):
+    """
+    Prints the output
+    """
+    uris = []
+    print('Recommended Songs:')
+    for i, j in enumerate(json_response['tracks']):
+        uris.append(j['uri'])
+        print(f"{i+1}) \"{j['name']}\" by {j['artists'][0]['name']}")
+
+def parse_options():
+    """Parse command line options."""
+    parser = argparse.ArgumentParser(description=("This is a command line interface (CLI) for "
+                                                  "the playlist_reccomendation module"),
+                                     epilog="Ethan Jones, 2020-05-25")
+    parser.add_argument("--seed-file", dest="seed_file", action="store", type=str,
+                        required=True, metavar="</path/to/file>",
+                        help="Specify the path to the seed file.")
+    options = parser.parse_args()
+    return options
+
+if __name__ == "__main__":
+    OPTIONS = parse_options()
+    main(OPTIONS.seed_file)
